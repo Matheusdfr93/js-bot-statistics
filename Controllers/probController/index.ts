ad718c9a20f1531ef66978c9634a4ae471e47d2f
Matheusdfr93 <matheusdfr93@gmail.com>
2020-11-07 15:54:21 -0300

    Rebaixamento(feat): create a function who bring the down chance

--- Controllers/probController/index.ts
+++ Controllers/probController/index.ts
@@ -1,15 +1,21 @@
 import { Request, Response } from "express";
 import champSerieA from "../../WebScraping/SerieA/Campeao";
+import libertSerieA from "../../WebScraping/SerieA/Libertadores";
+import rebaixamentoSerieA from "../../WebScraping/SerieA/Rebaixamento";
 
 const probController = {
   async probActions(req: Request, res: Response, next) {
     const acceptedActions = {
       findProbByTeam: async (params) => {
         const { team } = params;
-        const result = await champSerieA.getProbsByTeam(team);
+        const probChamp = await champSerieA.getProbsByTeam(team);
+        const probLibert = await libertSerieA.getProbsByTeam(team);
+        const probRebaix = await rebaixamentoSerieA.getProbsByTeam(team);
         return {
           valid: true,
-          result,
+          probChamp,
+          probLibert,
+          rebaixamento: probRebaix.prob,
         };
       },
     };
