9ff79b892f1534ffd1e0044fd2736d63b84abba2
Matheusdfr93 <matheusdfr93@gmail.com>
2020-11-07 21:00:08 -0300

    Serie B(feat): Create webscrapping to receiba serie b chance of champion

--- Controllers/probController/index.ts
+++ Controllers/probController/index.ts
@@ -2,23 +2,49 @@ import { Request, Response } from "express";
 import champSerieA from "../../WebScraping/SerieA/Campeao";
 import libertSerieA from "../../WebScraping/SerieA/Libertadores";
 import rebaixamentoSerieA from "../../WebScraping/SerieA/Rebaixamento";
+import champSerieB from "../../WebScraping/SerieB/Campeao";
 
 const probController = {
   async probActions(req: Request, res: Response, next) {
     const acceptedActions = {
-      findProbByTeam: async (params) => {
+      findProbByTeamSerieA: async (params) => {
         const { team } = params;
+        if (!team) {
+          return {
+            valid: false,
+          };
+        }
         const probChamp = await champSerieA.getProbsByTeam(team);
         const probLibert = await libertSerieA.getProbsByTeam(team);
         const probRebaix = await rebaixamentoSerieA.getProbsByTeam(team);
         return {
           valid: true,
-          probChamp,
-          probLibert,
-          rebaixamento: probRebaix.prob,
+          time: probChamp.time,
+          probabilidades: {
+            campeao: probChamp.prob,
+            libertadores: probLibert.prob,
+            rebaixamento: probRebaix.prob,
+          },
+        };
+      },
+      findProbByTeamSerieB: async (params) => {
+        const { team } = params;
+        if (!team) {
+          return {
+            valid: false,
+          };
+        }
+        const probChamp = await champSerieB.getProbsByTeam(team);
+        return {
+          valid: true,
+          time: probChamp.time,
+          probabilidades: {
+            campeao: probChamp.prob,
+          },
         };
       },
     };
+
     const { action } = req.body;
 
     const executeAction = await acceptedActions[action];
